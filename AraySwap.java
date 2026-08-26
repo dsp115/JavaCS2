320467dd3f1a33af85e2b4fe99bf2feeca8dff2b
dsp115 <dpinale1@my.epcc.edu>
2026-08-26 10:33:22 -0600

    Create AraySwap.java

---
+++ AraySwap.java
@@ -0,0 +1,12 @@
+public class AraySwap {
+        public static void main(String[] args) {
+            int[] array = {10, 4};
+
+            int temp = array[0];
+            array[0] = array[1];
+            array[1] = temp;
+
+            System.out.println("array[0] is " + array[0]);
+            System.out.println("array[1] is " + array[1]);
+        }
+}
