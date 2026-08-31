5bacfb912c133f8747f4bf5c586cf66732a893c3
dsp115 <dpinale1@my.epcc.edu>
2026-08-31 15:21:47 +0000

    learned enhanced

--- advancedArray.java
+++ advancedArray.java
@@ -1,1 +1,17 @@
+public class advancedArray {
+    public static void main(String[] args) {
+        int[] array = {-7,10,42,80};
+        int sum = 0;
+        for(int i = 0; i< array.length; i++) {
+            sum += array[i];
+        }
 
+        int[] ea = {4,24,80};
+
+        //       element--variableto hold an element of the array   array--literaly the array
+        for(int element : array){
+            sum+= element
+        }
+        // This does the same as the for int i=0 loop?
+    }
+}
