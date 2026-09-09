586b1e5d038369427188f80c016c876508c5b361
dsp115 <dpinale1@my.epcc.edu>
2026-09-09 15:40:31 +0000

    a

---
+++ crash.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class crash {
+    public static void main(String[]args) {
+        scnr Scanner = new Scanner(System.in);
+        in[] scores =  {90,85,76};
+        s
+    }
+    
+}
